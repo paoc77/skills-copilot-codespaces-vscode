5807f5f8de2c8723dcfe0338cd820b509abbb84d
Paolo Andres Olaya C <paolo.olaya@gmail.com>
2023-08-07 16:20:19 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
-function skillMembers(member) {
-    console.log(member);
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        calculateNumbers: function(var1, var2) {
+            console.log(var1, var2);
+        }
+    };
+    member.calculateNumbers(1, 2); // 1 2
+    console.log(member.name); // John
+    console.log(member.age); // 30
 }
